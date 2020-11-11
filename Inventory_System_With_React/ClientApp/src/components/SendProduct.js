44cf79ecb00a70d128c637668214229060e0eaa1
dhmec <dharmoradiya@gmail.com>
2020-11-11 13:47:04 -0700

    Add SendProduct Component

--- Inventory_System_With_React/ClientApp/src/components/SendProduct.js
+++ Inventory_System_With_React/ClientApp/src/components/SendProduct.js
@@ -1,1 +1,70 @@
-﻿
+﻿import React, { useState } from 'react';
+import axios from 'axios';
+
+function SendProduct(props) {
+    const [productID, setProductID] = useState();
+    const [quantity, setQuantity] = useState();
+    const [statusCode, setStatusCode] = useState(0);
+    const [response, setResponse] = useState([]);
+    const [waiting, setWaiting] = useState(false);
+
+    function handleFieldChange(event) {
+        switch (event.target.id) {
+            case "productID":
+                setProductID(event.target.value);
+                break;
+            case "quantity":
+                setQuantity(event.target.value);
+                break;
+        }
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setWaiting(true);
+
+        axios(
+            {
+                method: 'patch',
+                url: 'Inventory/Send',
+                params: {
+                    id: productID,
+                    quantity: quantity,
+                }
+            }
+        ).then((res) => {
+            setWaiting(false);
+            setResponse(res.data);
+            setStatusCode(res.status);
+           
+        }
+        ).catch((err) => {
+            setWaiting(false);
+            setResponse(err.response.data);
+            setStatusCode(err.response.status);
+        });
+
+        event.target.reset();
+      
+    }
+
+
+    return (
+
+        <div>
+            <h1>Send Product</h1>
+            <p>{waiting ? "Awaiting response..." : `Response recieved ${statusCode}: ${JSON.stringify(response)}`}</p>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="productID">Product ID</label>
+                <input id="productID" type="text" onChange={handleFieldChange} />
+                <label htmlFor="quantity">Product Quantity</label>
+                <input id="quantity" type="text" onChange={handleFieldChange} />
+                <input type="submit" value="Submit!" />
+            </form>
+        </div>
+
+
+    );
+}
+export { SendProduct };
